fbdf3176355a0fa12df8135f15eeda148b711706
Kevin0921a <yezhaokaiye@gmail.com>
2025-02-02 09:29:18 +0800

    寫了Coral 基本code

--- src/main/java/frc/robot/commands/CoralIntakeCommand.java
+++ src/main/java/frc/robot/commands/CoralIntakeCommand.java
@@ -1,5 +1,48 @@
 package frc.robot.commands;
 
-public class CoralIntakeCommand {
-    
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CoralSubsystem;
+
+
+public class CoralIntakeCommand extends Command {
+    private CoralSubsystem m_intake;
+    private Timer m_reverseTimer = new Timer();
+    private boolean isFinished;
+
+    public CoralIntakeCommand(CoralSubsystem m_intake){
+        this.m_intake = m_intake;
+        addRequirements(m_intake);
+    }
+
+    @Override
+    public void initialize(){
+        m_reverseTimer.reset();
+        isFinished = false;
+    }
+
+    @Override
+    public void execute(){
+        m_intake.Stop();
+        m_reverseTimer.start();
+
+        if(m_reverseTimer.get() > 0.3 && m_reverseTimer.get() < 0.5){
+            m_intake.ReverseMotor();
+        }
+
+        if(m_reverseTimer.get() > 0.5){
+            m_intake.Stop();
+            m_reverseTimer.stop();
+            isFinished = true;
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        m_intake.Stop();
+    }
+    @Override
+    public boolean isFinished(){
+        return isFinished;
+    }
 }
