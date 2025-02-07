c67ae03298f1097e7314a00e244cd9ff87638149
ALT <u11332280@lssh.tp.edu.tw>
2025-02-07 13:52:57 +0800

    改了光電的城市

--- src/main/java/frc/robot/commands/CoralIntakeReverseCommand.java
+++ src/main/java/frc/robot/commands/CoralIntakeReverseCommand.java
@@ -8,8 +8,6 @@ import frc.robot.subsystems.CoralSubsystem;
 public class CoralIntakeReverseCommand extends Command {
     private CoralSubsystem m_intake;
     private Timer m_reverseTimer = new Timer();
-    private boolean hasReversed;
-    private final double stopDistance = 200;
 
     public CoralIntakeReverseCommand(CoralSubsystem m_intake){
         this.m_intake = m_intake;
@@ -19,7 +17,6 @@ public class CoralIntakeReverseCommand extends Command {
     @Override
     public void initialize(){
         m_reverseTimer.reset();
-        hasReversed = false;
         m_reverseTimer.start();
     }
 
@@ -30,19 +27,17 @@ public class CoralIntakeReverseCommand extends Command {
             m_intake.setSpin(true);
         }
 
-        if(m_reverseTimer.get() > 0.2 && hasReversed){
-            m_intake.stop();
-            m_reverseTimer.stop();
+        if(m_reverseTimer.get() > 0.2 ){
+            m_intake.setSpin(false);
         }
     }
-
     @Override
-    public void end(boolean interrupted){
+    public void end(boolean interrupted) {
         m_intake.setSpin(false);
     }
 
     @Override
     public boolean isFinished(){
-        return m_reverseTimer.get() > 0.4 && m_intake.hasCoral();
+        return m_reverseTimer.get() > 0.3 && m_intake.hasCoral();
     }
 }            
